96440ca089bd2e8ed5dccd3781e5ab76a85290f8
Thomas Kiley <thomas.kiley@diffblue.com>
2019-01-02 18:25:07 +0000

    Crude first draft of the UI

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div id="root">
+				<div id="title-bar"><h1>Climate Dashboard</h1></div>
+				<div id="main">
+					<div id="nav-bar">
+						<ul className = "nav">
+							<li><a href="index">Dashboard</a></li>
+							<li><a href="emissions">Emissions</a></li>
+							<li><a href="carbon_budget">Carbon Budget</a></li>
+							<li><a href="contribubte">Contribubte</a></li>
+						</ul>
+					</div>
+					<div id="content">
+						<h2>Global Average Temperature</h2>
+						<p>Graph goes here</p>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
